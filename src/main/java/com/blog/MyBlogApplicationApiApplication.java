6dc047bbd0e981ba93812798131e783356a48fcb
Aniket Nikhade <nikhadeaniket81@gmail.com>
2023-11-15 17:16:20 +0530

    ADDEd method of His-301

--- src/main/java/com/blog/MyBlogApplicationApiApplication.java
+++ src/main/java/com/blog/MyBlogApplicationApiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyBlogApplicationApiApplication {
-
+private HashMap<String , Integer > hm = new HashMap()<>;
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogApplicationApiApplication.class, args);
 	}
 	
+
+public final String ="His-301"
 	@Bean
 	public ModelMapper modelmapper() {
 		return new ModelMapper();
